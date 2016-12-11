6d7cc417dde86ee6988d920ca220d77d9036206d
Arun Thekkuden <arun.thekkuden14@gmail.com>
2016-12-11 13:05:16 +0530

    minor edit

--- problems/problem_1.py
+++ problems/problem_1.py
@@ -4,8 +4,6 @@ https://projecteuler.net/problem=1
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
-
-Completed on Thu, 8 Dec 2016, 16:17
 '''
 
 import sys
